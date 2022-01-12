305a63b39c448ff624afc6e6c62d07ad028a8dac
Matthew Caren <matt.t.caren@gmail.com>
2022-01-11 22:50:25 -0500

    implement web audio API FFT

--- realtime-grapher.js
+++ realtime-grapher.js
@@ -9,12 +9,29 @@ function getSpec() {
     return document.specOut;
 }
 
+function getFFT() {
+    return document.fftOut;
+}
+
 
 RMS_GRAPH_DIV = document.getElementById('rmsGraph');
 SPEC_GRAPH_DIV = document.getElementById('specGraph');
+FFT_GRAPH_DIV = document.getElementById('fftGraph');
 
 rmsArray = Array(dataArrayLength).fill(undefined)
 specArray = Array(dataArrayLength).fill(undefined)
+
+var layout = {
+    title: 'FFT',
+    xaxis: {
+        type: 'log'
+    },
+    yaxis: {
+        range: [-200, 0],
+        autorange: false
+    }
+};
+
 
 Plotly.newPlot(RMS_GRAPH_DIV, [{
     y: [0, undefined],
@@ -34,6 +51,16 @@ Plotly.newPlot(SPEC_GRAPH_DIV, [{
     }
 }]);
 
+Plotly.newPlot(FFT_GRAPH_DIV, [{
+    y: [0, undefined],
+    mode: 'lines',
+    line: {
+        color: '#FC5656',
+        shape: 'linear'
+    },
+    log_x: true
+}], layout);
+
 
 var cnt = 0;
 
@@ -47,6 +74,8 @@ var interval = setInterval(function() {
         specArray = specArray.concat(newSpec)
         specArray.splice(0, 1)
 
+        var newFFT = getFFT();
+
         var rms_update = {
             y: [rmsArray]
         };
@@ -55,8 +84,13 @@ var interval = setInterval(function() {
             y: [specArray]
         };
 
+        var fft_update = {
+            y: newFFT
+        }
+
         Plotly.update('rmsGraph', rms_update)
         Plotly.update('specGraph', spec_update)
+        Plotly.update('fftGraph', fft_update)
     }
 
     if(cnt === 100) clearInterval(interval);
